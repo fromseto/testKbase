3b3f65d874dea34c9e3dc5485f15bcc0b2ac22eb
fromseto <wanglin44@gmail.com>
2017-02-06 11:46:39 -0500

    second commit

--- MyModule.spec
+++ MyModule.spec
@@ -6,4 +6,10 @@ module MyModule {
     /*
         Insert your typespec information here.
     */
+    typedef structure {
+	/*assembly_ref assembly_input_ref;*/
+	string workspace_name;
+	int min_length;
+    } FilterContigsParams;
 };
+
---
+++ lib/src/us/kbase/mymodule/FilterContigsParams.java
@@ -0,0 +1,80 @@
+
+package us.kbase.mymodule;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * <p>Original spec-file type: FilterContigsParams</p>
+ * <pre>
+ * Insert your typespec information here.
+ * </pre>
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("com.googlecode.jsonschema2pojo")
+@JsonPropertyOrder({
+    "workspace_name",
+    "min_length"
+})
+public class FilterContigsParams {
+
+    @JsonProperty("workspace_name")
+    private String workspaceName;
+    @JsonProperty("min_length")
+    private Long minLength;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("workspace_name")
+    public String getWorkspaceName() {
+        return workspaceName;
+    }
+
+    @JsonProperty("workspace_name")
+    public void setWorkspaceName(String workspaceName) {
+        this.workspaceName = workspaceName;
+    }
+
+    public FilterContigsParams withWorkspaceName(String workspaceName) {
+        this.workspaceName = workspaceName;
+        return this;
+    }
+
+    @JsonProperty("min_length")
+    public Long getMinLength() {
+        return minLength;
+    }
+
+    @JsonProperty("min_length")
+    public void setMinLength(Long minLength) {
+        this.minLength = minLength;
+    }
+
+    public FilterContigsParams withMinLength(Long minLength) {
+        this.minLength = minLength;
+        return this;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperties(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        return ((((((("FilterContigsParams"+" [workspaceName=")+ workspaceName)+", minLength=")+ minLength)+", additionalProperties=")+ additionalProperties)+"]");
+    }
+
+}
